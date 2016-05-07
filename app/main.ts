881f8aeab0d2da5d435817a9873466b4ffeaacc5
ChinCluBi <chincub@gmail.com>
2016-05-07 23:55:14 +0700

    Add rxjs and HTTP

--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,6 @@
-import { bootstrap }    from '@angular/platform-browser-dynamic';
-import { HomeComponent } from './components/home/home.component';
-bootstrap(HomeComponent);
+import 'rxjs/Rx';
+import { bootstrap }    from '@angular/platform-browser-dynamic'
+import { HTTP_PROVIDERS } from '@angular/http'
+
+import { HomeComponent } from './components/home/home.component'
+bootstrap(HomeComponent, [HTTP_PROVIDERS])
