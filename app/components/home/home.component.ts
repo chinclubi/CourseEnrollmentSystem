e8503a4888620b6adab0af6053507b55f5c31cc4
ChinCluBi <chincub@gmail.com>
2016-05-07 13:05:57 +0700

    - Add registration and information component - Config a router

--- app/components/home/home.component.ts
+++ app/components/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } 	from '@angular/core';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
 
+import { InformationComponent } from '../information/information.component';
+import { RegistrationComponent } from '../registration/registration.component';
 import { LoginComponent }	from '../login/login.component';
 import { SidebarComponent}  from '../../shared/directives/sidebar/sidebar.component';
 
@@ -14,11 +16,29 @@ import { User } from '../../shared/class/user';
 	providers: [ROUTER_PROVIDERS]
 })
 
+@RouteConfig([
+{
+	path: '/login',
+	name: 'Login',
+	component: LoginComponent,
+	useAsDefault: true
+},
+{
+	path: '/infomation',
+	name: 'Information',
+	component: InformationComponent
+},
+{
+	path: '/registration',
+	name: 'Registration',
+	component: RegistrationComponent
+}
+])
+
 export class HomeComponent implements OnInit {
 	currentUser = new User('', false);
 
 	ngOnInit() {
-		console.log(localStorage.getItem('user'))
 		if (localStorage.getItem('user')){
 			this.currentUser.id = localStorage.getItem('user')
 			this.currentUser.isLogin = true
---
+++ app/components/information/information.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "information",
+	template: "This page is information"
+})
+
+export class InformationComponent {}
