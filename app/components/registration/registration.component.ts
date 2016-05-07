cc7d2bc6ec9bff819ca9dab46417f05f37c253da
ChinCluBi <chincub@gmail.com>
2016-05-08 04:40:15 +0700

    Output pipe directive

--- app/components/registration/registration.component.ts
+++ app/components/registration/registration.component.ts
@@ -3,20 +3,31 @@ import { Component, OnInit} from '@angular/core'
 import { Course } from '../../shared/class/course'
 import { CourseService } from '../../shared/services/course.service'
 
+import { SearchBarComponent}  from '../../shared/directives/sticky/search.component'
+import { SearchPipe } from '../../shared/filters/search.filter'
+
 @Component({
 	selector: 'registration',
 	templateUrl: 'build/views/registration.component.html',
-	styleUrls: ['build/styles/registration.component.css']
+	styleUrls: ['build/styles/registration.component.css'],
+	directives: [SearchBarComponent],
+	pipes: [SearchPipe]
 })
 
 export class RegistrationComponent implements OnInit {
 	courses: Course[]
+	keywords: string
+	isEmpty: boolean
 	constructor(
 		private courseService: CourseService
 	) {}
 
 	ngOnInit() {
 		this.courses = this.courseService.getCourse()
-		console.log(this.courses)
+		this.isEmpty = false
+	}
+
+	onKeywordChange(keyword) {
+		this.keywords = keyword
 	}
 }
