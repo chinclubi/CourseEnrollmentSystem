907f254c5c8e419c059750d6c75ad2944ca20d3f
ChinCluBi <chincub@gmail.com>
2016-05-08 16:30:58 +0700

    Create enroll class

---
+++ app/components/enroll/enroll.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+	selector: 'enroll-sidebar',
+	templateUrl: 'build/views/enroll.component.html'
+})
+
+export class EnrollComponent{}
---
+++ app/shared/class/enroll.ts
@@ -0,0 +1,6 @@
+export class Enroll {
+	constructor(
+		public courseId: string,
+		public secId: string
+	) { }
+}
