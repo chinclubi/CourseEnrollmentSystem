4c7a98f67d01eac22f85240475f4b698b10c9b37
ChinCluBi <chincub@gmail.com>
2016-05-10 05:16:48 +0700

    Show export json data and edit modal style

--- app/shared/directives/sidebar/sidebar.component.ts
+++ app/shared/directives/sidebar/sidebar.component.ts
@@ -1,25 +1,37 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, AfterViewInit, ElementRef } from '@angular/core'
 import { Router } from '@angular/router-deprecated'
 
 import { User } from '../../class/user'
 import { UserService } from '../../services/user.service'
 import { SearchService } from '../../services/search.service'
+import { EnrollService } from '../../services/enroll.service'
+
+declare var jQuery: any
 
 @Component({
 	selector: 'sidebar',
 	templateUrl: 'build/views/sidebar.component.html'
 })
 
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, AfterViewInit {
 	currentUser: User
+	modalJson: any
+	jsonDataString: any
 
 	constructor(
 		private router: Router,
 		private userService: UserService,
-		private searchService: SearchService) { }
+		private searchService: SearchService,
+		private enrollService: EnrollService,
+		private elRef: ElementRef) { }
 
 	ngOnInit() {
 		this.currentUser = this.userService.getUser()
+		this.getEnrollData()
+	}
+
+	ngAfterViewInit() {
+		this.modalJson = jQuery(this.elRef.nativeElement).find('.ui.modal')
 	}
 
 	onSubmit() {
@@ -44,5 +56,17 @@ export class SidebarComponent implements OnInit {
 	goToRegistration() {
 		this.searchService.clear()
 		this.router.navigate(['Registration', {}]);
+	}
+
+	exportJson(){
+		this.modalJson
+		.modal({
+			blurring: true
+		})
+		.modal('show')
+	}
+
+	getEnrollData() {
+		this.jsonDataString = JSON.stringify(this.enrollService.getList(), undefined, 2)
 	}
 }
