ec4337029cefbb061372d427249add0ea70a2c47
ChinCluBi <chincub@gmail.com>
2016-05-09 15:50:44 +0700

    Init drop btn

--- app/components/enroll/enroll.component.ts
+++ app/components/enroll/enroll.component.ts
@@ -1,24 +1,37 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, ElementRef, AfterViewInit } from '@angular/core'
 import { Router } from '@angular/router-deprecated'
 
 import { Enroll } from '../../shared/class/enroll'
 import { EnrollService } from '../../shared/services/enroll.service'
+import { DropComponent } from '../../shared/directives/drop/drop.component'
+
+declare var jQuery: any
 
 @Component({
 	selector: 'enroll-sidebar',
-	templateUrl: 'build/views/enroll.component.html'
+	templateUrl: 'build/views/enroll.component.html',
+	directives: [DropComponent]
 })
 
-export class EnrollComponent implements OnInit {
+export class EnrollComponent implements OnInit, AfterViewInit {
 	enrollList: Enroll[]
 	credit: any
 
 	constructor(
 		private enrollService: EnrollService,
-		private router: Router
+		private router: Router,
+		private elRef: ElementRef
 	){
 		this.credit = 0
 		this.enrollList = []
+	}
+
+	ngAfterViewInit(){
+		jQuery(this.elRef.nativeElement).find('.ui.sticky')
+			.sticky({
+				context: '#content',
+				observeChanges: true
+			})
 	}
 
 	ngOnInit() {
